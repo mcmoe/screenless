4b2bff327b75abffd2a314ed14a6589064f77ce2
mcmoe <mcxmoe@gmail.com>
2022-08-22 17:26:05 +0200

    Add T2S example using Mozilla TTS and info on Google Cloud T2S

---
+++ examples/t2s.js
@@ -0,0 +1,32 @@
+import 'dotenv-expand/config';
+import Sound from 'aplay';
+import got from 'got';
+import {promisify} from 'node:util';
+import stream from 'node:stream';
+import fs from 'node:fs';
+
+const speechThat = async (text) => {
+  const file = `${process.env.AUDIO_DIR}tmp.wav`;
+  const pipeline = promisify(stream.pipeline);
+  await pipeline(
+    got.stream(`http://localhost:5002/api/tts?text=${text}`),
+    fs.createWriteStream(file)
+  );
+  return file;
+}
+
+const playThat = (file) => {
+  return new Promise((resolve, reject) => {
+    const sound = new Sound()
+    sound.on('complete', (code, sig) => {
+      console.log('[DatTing] play completed -', '[code]:', code, ' [sig]:', sig);
+      resolve();
+    });
+    sound.play(file);
+  });
+};
+
+export const speak = (text) => {
+    const file = await speechThat(text);
+    await playThat(file);
+}
